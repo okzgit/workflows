cf8b3f13f66228b21be91cacfdcb023aa146bfac
o kz <ouriakz@outlook.com>
2016-04-09 12:59:04 +0200

    browserify and mustach has added.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     concat = require('gulp-concat'),
+    browserify = require('gulp-browserify'),
     conu = require('gulp-concat-util');
 
 var jsSources = ['components/scripts/rclick.js',
@@ -30,6 +31,7 @@ gulp.task('coffee', function() {
 gulp.task('js', function() {
    gulp.src(jsSources)
     .pipe(concat('script.js'))
+     .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
 });
 
