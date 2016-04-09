c646629c51ff11ca959854e909f1b437335672ea
o kz <ouriakz@outlook.com>
2016-04-09 12:32:43 +0200

    JavaScript files has used concat-util

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat'),
+    conu = require('gulp-concat-util');
+
+var jsSources = ['components/scripts/rclick.js',
+                 'components/scripts/pixgrid.js',
+                 'components/scripts/tagline.js',
+                 'components/scripts/template.js'
+                ];
 
 
 gulp.task('log', function () {
@@ -12,8 +20,21 @@ gulp.task('log', function () {
 var coffeeSource = ['components/coffee/tagline.coffee'];
 gulp.task('coffee', function() {
     
-        gulp.src(coffeeSource)
+        gulp.src(coffeeSource
+                )
         .pipe(coffee({bare: true })
              .on('error', gutil.log ))
               .pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function() {
+   gulp.src(jsSources)
+    .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('jss', function() {
+   return gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('builds/development/js')); 
 });
