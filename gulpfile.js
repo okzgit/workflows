2e821452c1c6ee671eb626981028dda72bf1c72b
o kz <ouriakz@outlook.com>
2016-04-09 11:46:07 +0200

    CoffeeScript files has added.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
 
 
 gulp.task('log', function () {
   
     gutil.log('It is working!');
     
-}); 
+});
+
+var coffeeSource = ['components/coffee/tagline.coffee'];
+gulp.task('coffee', function() {
+    
+        gulp.src(coffeeSource)
+        .pipe(coffee({bare: true })
+             .on('error', gutil.log ))
+              .pipe(gulp.dest('components/scripts'))
+});
